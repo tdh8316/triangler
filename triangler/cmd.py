bf0143467a7bab4bea4f97d9e8d1b42283f76f29
탁동혁 <tdh8316@naver.com>
2020-02-21 12:08:51 +0900

    core: Add multiprocessing support

--- triangler/cmd.py
+++ triangler/cmd.py
@@ -1,6 +1,10 @@
+import multiprocessing
+import time
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from pathlib import Path
+from typing import List
 
+from numpy.core.multiarray import ndarray
 from skimage.io import imsave
 
 from triangler.color import ColorMethod
@@ -10,47 +14,79 @@ from triangler.sampling import SampleMethod
 
 
 # noinspection PyProtectedMember
-def main():
+def main() -> None:
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("image", help="Source file", type=str)
-    parser.add_argument("-o", help="Destination file", type=str, default=None)
+    parser.add_argument("images", help="Source files", nargs="+", type=str)
+    parser.add_argument(
+        "-o", "--output", help="Destination file", nargs="+", type=str, default=None
+    )
     parser.add_argument(
         "-s",
         "--sample",
         help="Sampling method for candidate points.",
-        type=str,
-        default="threshold",
+        type=str.upper,
+        default="THRESHOLD",
         choices=SampleMethod._member_names_,
     )
     parser.add_argument(
         "-e",
         "--edge",
         help="Pre-processing method to use.",
-        type=str,
-        default="sobel",
+        type=str.upper,
+        default="SOBEL",
         choices=EdgeMethod._member_names_,
     )
     parser.add_argument(
         "-c",
         "--color",
         help="Coloring method for rendering.",
-        type=str,
-        default="centroid",
+        type=str.upper,
+        default="CENTROID",
         choices=ColorMethod._member_names_,
     )
     parser.add_argument(
-        "-p", "--points", help="Points threshold.", type=int, default=4096
+        "-p", "--points", help="Points threshold.", type=int, default=1024
     )
 
     args = parser.parse_args()
 
-    result = process(
-        path=args.image,
-        coloring=ColorMethod[args.color.upper()],
-        sampling=SampleMethod[args.sample.upper()],
-        edging=EdgeMethod[args.edge_detector.upper()],
-        points=args.points,
+    calls: List[multiprocessing.Process] = []
+
+    for image in args.images:
+        _process = multiprocessing.Process(
+            target=spawn,
+            args=(
+                image,
+                None,
+                ColorMethod[args.color.upper()],
+                SampleMethod[args.sample.upper()],
+                EdgeMethod[args.edge.upper()],
+                args.points,
+            ),
+        )
+        calls.append(_process)
+        _process.start()
+
+    for func in calls:
+        func.join()
+
+
+def spawn(
+    img_path: str,
+    out_path: str or None,
+    c: ColorMethod,
+    s: SampleMethod,
+    e: EdgeMethod,
+    p: int,
+) -> None:
+    print("Spawned " + img_path)
+    start_time = time.time()
+    res: ndarray = process(
+        path=img_path, coloring=c, sampling=s, edging=e, points=p,
     )
 
-    imsave(args.o or (Path(args.image).name + "_tri.jpg"), result)
+    imsave(
+        out_path or (str().join(Path(img_path).name.split(".")[:-1]) + "_tri.jpg"), res
+    )
+    print("Finished " + img_path + " in {}s".format(time.time() - start_time))
