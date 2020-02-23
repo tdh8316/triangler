a96c3bbb6abc2b4e09170df0f56657ed53baeb48
탁동혁 <tdh8316@naver.com>
2020-02-23 15:16:04 +0900

    core: Fix default name of the result file

--- triangler/cmd.py
+++ triangler/cmd.py
@@ -1,7 +1,8 @@
+import logging
 import multiprocessing
+import sys
 import time
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-from pathlib import Path
 from typing import List
 
 from numpy.core.multiarray import ndarray
@@ -49,28 +50,55 @@ def main() -> None:
         "-p", "--points", help="Points threshold.", type=int, default=1024
     )
 
+    parser.add_argument(
+        "-v", "--verbose", help="Set logger level as DEBUG", action="store_true"
+    )
+
     args = parser.parse_args()
 
-    # Use multiprocessing to process multiple files at the same time
-    calls: List[multiprocessing.Process] = []
+    # Set logger
+    logging.basicConfig(
+        level=logging.DEBUG if args.verbose else logging.WARNING,
+        format="[%(asctime)s][%(levelname)s] in %(funcName)s(): %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        stream=sys.stdout,
+    )
 
-    for image in args.images:
-        _process = multiprocessing.Process(
-            target=spawn,
-            args=(
-                image,
-                None,
-                ColorMethod[args.color.upper()],
-                SampleMethod[args.sample.upper()],
-                EdgeMethod[args.edge.upper()],
-                args.points,
-            ),
+    logging.info("Options:{}".format(args))
+
+    if args.output and len(args.images) != len(args.output):
+        raise IndexError
+
+    if len(args.images) > 1:
+        # Use multiprocessing to process multiple files at the same time
+        calls: List[multiprocessing.Process] = []
+        for index, image in enumerate(args.images):
+            _process = multiprocessing.Process(
+                target=spawn,
+                args=(
+                    image,
+                    None if not args.output else args.output[index],
+                    ColorMethod[args.color.upper()],
+                    SampleMethod[args.sample.upper()],
+                    EdgeMethod[args.edge.upper()],
+                    args.points,
+                ),
+            )
+            calls.append(_process)
+            _process.daemon = True
+            _process.start()
+
+        for func in calls:
+            func.join()
+    else:
+        spawn(
+            args.images[0],
+            None if not args.output else args.output[0],
+            ColorMethod[args.color.upper()],
+            SampleMethod[args.sample.upper()],
+            EdgeMethod[args.edge.upper()],
+            args.points,
         )
-        calls.append(_process)
-        _process.start()
-
-    for func in calls:
-        func.join()
 
 
 def spawn(
@@ -81,13 +109,15 @@ def spawn(
     e: EdgeMethod,
     p: int,
 ) -> None:
-    print("Spawned " + img_path)
+    logging.info("Spawned process for {}".format(img_path))
     start_time = time.time()
     res: ndarray = process(
         path=img_path, coloring=c, sampling=s, edging=e, points=p,
     )
 
     imsave(
-        out_path or (str().join(Path(img_path).name.split(".")[:-1]) + "_tri.jpg"), res
+        out_path
+        or (str().join(img_path.split(".")[:-1]) + "_tri." + img_path.split(".")[-1]),
+        res,
     )
-    print("Finished " + img_path + " in {}s".format(time.time() - start_time))
+    logging.info("Finished {} in {}s".format(img_path, time.time() - start_time))
