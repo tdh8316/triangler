36188088f00dba70477aec159fd859e6bae99cf7
탁동혁 <tdh8316@naver.com>
2020-02-26 14:36:54 +0900

    Add required python version

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
+with open("README.md") as fh:
+    long_description = fh.read()
+
 setup(
     name="triangler",
-    version="0.1",
+    version="0.2",
     packages=["triangler"],
     url="https://github.com/tdh8316/triangler",
     license="MIT License",
     author="Donghyeok Tak",
     author_email="tdh8316@naver.com",
     description="Convert images to Low-Poly art using Delaunay triangulation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=["scikit-image", "numpy", "scipy", "numba", "imageio"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
