a57d3790eabe792a18d1dfede5fe7064311f803d
탁동혁 <tdh8316@naver.com>
2020-03-14 16:14:24 +0900

    Add example.py

---
+++ examples/example.py
@@ -0,0 +1,21 @@
+from skimage.io import imread
+
+import triangler
+
+import matplotlib.pyplot as plt
+
+# Read image from disk
+img = imread("../test/piano.jpg")
+
+# Create Triangler instance
+t = triangler.Triangler(sample_method=triangler.SampleMethod.POISSON_DISK)
+
+print("Converting... ")
+# Convert
+img_tri = t.convert(img)
+print("[Done]")
+
+_, axes = plt.subplots(1, 2, figsize=(16, 16))
+axes[0].imshow(img)
+axes[1].imshow(img_tri)
+plt.show()
