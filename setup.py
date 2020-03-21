237d745a1d2b1e6d962e933529a98d7097759a8c
탁동혁 <tdh8316@naver.com>
2020-03-21 14:37:00 +0900

    Fix missing comma in install requires

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        "matplotlib"
+        "matplotlib",
         "scikit-image",
         "numpy==1.17.2",
         "scipy==1.4.1",
