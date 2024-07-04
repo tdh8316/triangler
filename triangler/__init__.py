123c91447693e6f40340568266b166784210a60b
Donghyeok Tak <tdh8316@naver.com>
2024-07-05 02:40:01 +0900

    Fix imports

--- triangler/__init__.py
+++ triangler/__init__.py
@@ -1,4 +1,7 @@
 from triangler.converter import convert
-
+from triangler.config import TrianglerConfig
+from triangler.edge_detectors import EdgeDetector
+from triangler.renderers import Renderer
+from triangler.samplers import Sampler
 
 __version__ = "0.9.2"
